855f22ab5002ddbaecc16d026bb44a7572561dfb
Kim Toy (Personal) <tokutei12@gmail.com>
2017-02-11 16:45:00 -0800

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by Kim Toy (Personal) on 2/11/17.
+//  Copyright © 2017 Kim Toy (Personal). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
